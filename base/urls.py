64a201e6f05a8a7fa3d7952b768debcbd6720d47
SciKa <enzoescipy@gmail.com>
2020-09-18 19:14:26 +0900

    api added : poster

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import datashower
+from .views import datashower, dataposter
 
 urlpatterns = [
     path("shower/", datashower),
+    path("poster/", dataposter),
 ]
--- base/views.py
+++ base/views.py
@@ -2,9 +2,18 @@ from rest_framework.response import  Response
 from rest_framework.decorators import api_view
 from .models import GPSTime
 from .serializers import GPSTimeSerializer
+from rest_framework import status
 
 @api_view(['GET'])
 def datashower(request):
     totaldatas = GPSTime.objects.all()
     serializer = GPSTimeSerializer(totaldatas, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def dataposter(request):
+    serializer = GPSTimeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
